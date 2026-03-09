0b61420987ec22dea900c092849a2ca3f36d4895
sist-25 <sist-25@sist25>
2026-03-09 17:33:48 +0900

    commit

---
+++ Chapter01_자바시작/src/자바_구동.java
@@ -0,0 +1,27 @@
+/*
+ * 일반 프로그램
+ * 
+ *  프로그램
+ *    |
+ *  운영체제
+ *    |
+ *  하드웨어
+ *  
+ * 자바 프로그램
+ * 
+ *  프로그램
+ *    |
+ *   JVM : JDK => 실행속도가 C언어보다는 늦다(네트워크,파일)
+ *    |
+ *  운영체제
+ *    |  
+ *  하드웨어
+ * 
+ * 
+ * */
+public class 자바_구동 {
+	public static void main(String[] args) {
+		System.out.println("자바 실행");
+		System.out.println("<html><body><h1>Hello JAVA</h1></body></html>");
+	}
+}
---
+++ Chapter01_자바시작/src/자바_시작.java
@@ -0,0 +1,22 @@
+/*
+ *  자바 => 모든 소스는 클래스안에 첨부
+ *  	=> JVM이 가장먼저 실행하는 부분은 main (진입점)
+ *  
+ *  이미 만들어진 클래스 읽기 => import 쓰기
+ *  package
+ *  import
+ *  public class ClassName{
+ *  	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		}
+ *  }
+ * */
+public class 자바_시작 {
+	
+	 static public void main(String[] ddsfsfd) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello Java");
+	}
+
+}
